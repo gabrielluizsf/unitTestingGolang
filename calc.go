c82b0d1e61d5f54acdc58caa723d77a8be4ba9cb
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-02-24 10:23:40 -0300

    functions created

---
+++ calc.go
@@ -0,0 +1,17 @@
+package unitTestingGolang
+
+func Add(number, number2 int)int{
+	return number + number2;
+}
+
+func Sub(number, number2 int)int{
+	return number - number2;
+}
+
+func Mult(number, number2 int) int{
+	return number * number2;
+}
+
+func Div(number, number2 int) int{
+	return number / number2;
+}
